f5b097bee96e6b1db9d42f34c2016076ab510757
burnroth <marcus,bernroth@lime.tech>
2019-08-27 07:16:07 +0200

    redux boilerplate

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,20 @@
-import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css'
+import React from "react";
+import { Provider } from "react-redux";
+import store from "./store";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-import './App.css';
-import AppNavbar from './components/AppNavbar';
-import ShoppingList from './components/ShoppingList';
+import "./App.css";
+import AppNavbar from "./components/AppNavbar";
+import ShoppingList from "./components/ShoppingList";
 
 function App() {
   return (
-    <div className="App">
-
-      <AppNavbar/>
-      <ShoppingList/>
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <AppNavbar />
+        <ShoppingList />
+      </div>
+    </Provider>
   );
 }
 
