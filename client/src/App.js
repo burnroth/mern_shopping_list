88f4b2bf6ab94a1d73dcb58c2123f337d6af6997
burnroth <marcus,bernroth@lime.tech>
2019-08-28 06:49:08 +0200

    added react-helmet

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,7 @@ import { Provider } from "react-redux";
 import store from "./store";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Container } from 'reactstrap'
-
+import { Helmet } from 'react-helmet'
 import "./App.css";
 import AppNavbar from "./components/AppNavbar";
 import ShoppingList from "./components/ShoppingList";
@@ -12,6 +12,9 @@ import ItemModal from './components/itemModal'
 function App() {
   return (
     <Provider store={store}>
+      <Helmet title="Marcus learnig the MERN stack">
+
+      </Helmet>
       <div className="App">
         <AppNavbar />
         <Container>
