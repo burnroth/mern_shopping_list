bcb05c608c9ec39292bf7b25e45546d02b78b816
burnroth <marcus,bernroth@lime.tech>
2019-09-05 18:32:02 +0200

    heroku

--- routes/api/alba.js
+++ routes/api/alba.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const fetch = require("node-fetch");
 
-router.post("/api/alba", (req, res) => {
+router.post("/", (req, res) => {
   const endpoint =
     "https://prod-113.westeurope.logic.azure.com:443/workflows/a35ed98a961b4c47844a2dd94a2986a2/triggers/manual/paths/invoke?api-version=2016-06-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=ywmBb_SCkBOC68XzwqxcdL7PmGr4Eeq2Kj9b5b9oxCU";
   fetch(endpoint, {
