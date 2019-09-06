49b8043b7b85eb1b143722f8bdebb32ef5ff1c8c
burnroth <marcus,bernroth@lime.tech>
2019-09-06 13:32:43 +0200

    switched from node-fetch to axios

--- routes/api/alba.js
+++ routes/api/alba.js
@@ -1,20 +1,22 @@
 const express = require("express");
 const router = express.Router();
 const fetch = require("node-fetch");
+const axios = require("axios")
 
 router.post("/", (req, res) => {
-  const segment =
-    "https://fn.segmentapis.com/?b=TjdxQzQ2dDVYSTo6ZjNBOEJsVjdKV2JMb2hXUXdveFROQVBQcWVmamxJVlU=";
-  fetch(segment, {
-    method: "POST",
-    headers: {
-      "Content-Type" : "application/json"
-    },
-    body: req.body
+
+  axios.post('https://fn.segmentapis.com/?b=TjdxQzQ2dDVYSTo6ZjNBOEJsVjdKV2JMb2hXUXdveFROQVBQcWVmamxJVlU=', {
+    ...req.body
   })
-    .then(() => res.json({ success: true }))
-    .then(console.log(req.body))
-    .catch(err => res.json({ success: false }, console.log(err)));
+  .then(function (response) {
+    console.log(response);
+  })
+  .then(() => res.json({ success: true }))
+  .catch(function (error) {
+    console.log(error);
+  });
+
+
 });
 
 module.exports = router;
