afcae03c6485841dfbf68c9bf8f145c4db87e088
burnroth <marcus,bernroth@lime.tech>
2019-10-01 16:07:40 +0200

    ttt

--- routes/api/alba.js
+++ routes/api/alba.js
@@ -8,7 +8,7 @@ router.post("/", (req, res) => {
     ...req.body
   })
   .then(function (response) {
-    console.log(response.config.data);
+    console.log(JSON.stringify(response.config.data));
   })
   .then(() => res.json({ success: true }))
   .catch(function (error) {
